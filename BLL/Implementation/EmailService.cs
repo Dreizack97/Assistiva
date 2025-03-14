7349ac0d5d1746955560b2c393a901ef9ed81e62
Dreizack97 <dreizack97@gmail.com>
2025-03-13 19:52:50 -0600

    Desarrollo del servicio de email.

--- BLL/Implementation/EmailService.cs
+++ BLL/Implementation/EmailService.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BLL.Interfaces;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace BLL.Implementation
 {
-    class EmailService
+    public class EmailService : IEmailService
     {
+        public struct MailServer
+        {
+            public const string ADDRESS = "no.reply@nukabe.mx";
+
+            public const string PASSWORD = "SbrxTo59AQ&K";
+
+            public const string DISPLAY_NAME = "Assistiva";
+
+            public const string HOST = "mail.nukabe.mx";
+
+            public const int PORT = 587;
+
+            public const bool ENABLE_SSL = true;
+        }
+
+        public async Task<bool> SendEmailAsync(string addresses, string subject, string body, List<string>? attachments = null)
+        {
+            return await SendEmailAsync(new List<string> { addresses }, subject, body, attachments);
+        }
+
+        public async Task<bool> SendEmailAsync(List<string> addresses, string subject, string body, List<string>? attachments = null)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress(MailServer.DISPLAY_NAME, MailServer.ADDRESS));
+
+            foreach (string address in addresses)
+                message.To.Add(MailboxAddress.Parse(address));
+
+            message.Subject = subject;
+
+            BodyBuilder builder = new BodyBuilder { HtmlBody = body };
+
+            if (attachments != null)
+                foreach (string attachment in attachments)
+                    if (!string.IsNullOrWhiteSpace(attachment))
+                        builder.Attachments.Add(attachment);
+
+            message.Body = builder.ToMessageBody();
+
+            using (SmtpClient smtpClient = new SmtpClient())
+            {
+                try
+                {
+                    await smtpClient.ConnectAsync(MailServer.HOST, MailServer.PORT, MailServer.SSL);
+                    await smtpClient.AuthenticateAsync(MailServer.ADDRESS, MailServer.PASSWORD);
+                    await smtpClient.SendAsync(message);
+                    await smtpClient.DisconnectAsync(true);
+                    return true;
+                }
+                catch (SmtpCommandException)
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
