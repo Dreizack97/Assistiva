16a1c3330e5cbf1a9d5c2a7ae5ae06d15ba7ad40
Dreizack97 <dreizack97@gmail.com>
2025-03-25 20:00:28 -0600

    Creación y configuración de 'Areas' para separar responsabilidades.

---
+++ AppUI/Areas/School/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppUI.Areas.School.Controllers
+{
+    [Authorize]
+    [Area("School")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
