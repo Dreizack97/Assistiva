71a6e895fa44b7802539221167f3cdb0b626e052
Dreizack97 <dreizack97@gmail.com>
2025-03-26 15:58:48 -0600

    Creación de 'DisabilitiesController'.

---
+++ AppUI/Areas/School/Controllers/DisabilitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppUI.Areas.School.Controllers
+{
+    [Authorize]
+    [Area("School")]
+    public class DisabilitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+    }
+}
