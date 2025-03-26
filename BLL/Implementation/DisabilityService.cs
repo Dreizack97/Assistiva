072f5193fd3bc42435e13da759187a1014aba123
Dreizack97 <dreizack97@gmail.com>
2025-03-26 15:36:20 -0600

    Diseño de interfaz e implementación de 'DisabilityService'.

---
+++ BLL/Implementation/DisabilityService.cs
@@ -0,0 +1,41 @@
+﻿using BLL.Interfaces;
+using DAL.Interfaces;
+using Entity;
+
+namespace BLL.Implementation
+{
+    public class DisabilityService : IDisabilityService
+    {
+        private readonly IGenericRepository<Disability> _repository;
+
+        public DisabilityService(IGenericRepository<Disability> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Disability> CreateAsync(Disability disability)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Disability> GetByIdAsync(int disabilityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Disability>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Disability disability)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DisableAsync(int disabilityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Interfaces/IDisabilityService.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+
+namespace BLL.Interfaces
+{
+    public interface IDisabilityService
+    {
+        Task<Disability> CreateAsync(Disability disability);
+
+        Task<Disability> GetByIdAsync(int disabilityId);
+
+        Task<IEnumerable<Disability>> GetAllAsync();
+
+        Task<bool> UpdateAsync(Disability disability);
+
+        Task<bool> DisableAsync(int disabilityId);
+    }
+}
