4e3646929efe4991e2ee3420fa3b6ccf3fff2815
Dreizack97 <dreizack97@gmail.com>
2025-03-26 20:37:05 -0600

    Diseño de interfaz e implementación de 'StudentService'.

---
+++ BLL/Implementation/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using BLL.Interfaces;
+using DAL.Interfaces;
+using Entity;
+
+namespace BLL.Implementation
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IGenericRepository<Student> _repository;
+        private readonly IUserService _userService;
+
+        public StudentService(IGenericRepository<Student> repository, IUserService userService)
+        {
+            _repository = repository;
+            _userService = userService;
+        }
+
+        public Task<Student> CreateAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetByIdAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Student>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DisableAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using Entity;
+
+namespace BLL.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<Student> CreateAsync(Student student);
+        Task<Student> GetByIdAsync(int studentId);
+        Task<IEnumerable<Student>> GetAllAsync();
+        Task<bool> UpdateAsync(Student student);
+        Task<bool> DisableAsync(int studentId);
+    }
+}
