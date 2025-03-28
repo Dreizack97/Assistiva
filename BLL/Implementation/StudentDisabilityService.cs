30cccaa766573c865ed18799d3597b6376ea7a5a
Dreizack97 <dreizack97@gmail.com>
2025-03-27 21:11:19 -0600

    Desarrollo de la implementación de 'StudentDisabilityService'. Documentación de la interfaz e implementación.

--- BLL/Implementation/StudentDisabilityService.cs
+++ BLL/Implementation/StudentDisabilityService.cs
@@ -4,38 +4,89 @@ using Entity;
 
 namespace BLL.Implementation
 {
+    /// <inheritdoc cref="IStudentDisabilityService"/>
     public class StudentDisabilityService : IStudentDisabilityService
     {
         private readonly IGenericRepository<StudentDisability> _repository;
 
+        /// <summary>
+        /// Inicializa una nueva instancia del servicio con el repositorio especificado.
+        /// </summary>
+        /// <param name="repository">Repositorio para acceder a los datos de StudentDisability.</param>
         public StudentDisabilityService(IGenericRepository<StudentDisability> repository)
         {
             _repository = repository;
         }
-
-        public Task<StudentDisability> CreateAsync(StudentDisability studentDisability)
+        
+        /// <inheritdoc />
+        /// <exception cref="TaskCanceledException">
+        /// Se lanza cuando:
+        /// <list type="bullet">
+        /// <item>Ya existe una asociación idéntica para el estudiante</item>
+        /// <item>Ocurre un error durante la creación del registro</item>
+        /// </list>
+        /// </exception>
+        public async Task<StudentDisability> CreateAsync(StudentDisability studentDisability)
         {
-            throw new NotImplementedException();
+            StudentDisability? oStudentDisability = await _repository.GetByFilterAsync(d => d.StudentId == studentDisability.StudentId && d.DisabilityId == studentDisability.DisabilityId);
+
+            if (oStudentDisability != null)
+                throw new TaskCanceledException("El estudiante ya cuenta una esta discapacidad registrada.");
+
+            StudentDisability _studentDisability = await _repository.AddAsync(studentDisability);
+
+            if (_studentDisability.Id == 0)
+                throw new TaskCanceledException("Ocurrió un error al intentar registrar la discapacidad.");
+
+            return _studentDisability;
         }
 
-        public Task<StudentDisability> GetByIdAsync(int id)
+        /// <inheritdoc />
+        /// <exception cref="TaskCanceledException">Se lanza cuando no se encuentra el registro con el ID especificado.</exception>
+        public async Task<StudentDisability> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            StudentDisability? studentDisability = await _repository.GetByIdAsync(id)
+                ?? throw new TaskCanceledException("No se encontró registro con la información proporcionada.");
+
+            return studentDisability;
         }
 
-        public Task<IEnumerable<StudentDisability>> GetAllByStudentIdAsync(int studentId)
+        /// <inheritdoc />
+        /// <returns>
+        /// Una colección de <see cref="StudentDisability"/> que puede estar vacía si no se encuentran resultados.
+        /// </returns>
+        public async Task<IEnumerable<StudentDisability>> GetAllByStudentIdAsync(int studentId)
         {
-            throw new NotImplementedException();
+            return await _repository.GetAllAsync(d => d.StudentId == studentId);
         }
 
-        public Task<bool> UpdateAsync(StudentDisability studentDisability)
+        // <inheritdoc />
+        /// <exception cref="TaskCanceledException">
+        /// Se lanza cuando:
+        /// <list type="bullet">
+        /// <item>No se encuentra el registro a actualizar</item>
+        /// <item>Ya existe otra asociación idéntica para el estudiante</item>
+        /// </list>
+        /// </exception>
+        public async Task<bool> UpdateAsync(StudentDisability studentDisability)
         {
-            throw new NotImplementedException();
+            StudentDisability? oStudentDisability = await _repository.GetByFilterAsync(d => d.StudentId == studentDisability.StudentId && d.DisabilityId == studentDisability.DisabilityId 
+                && d.Id != studentDisability.Id);
+
+            if (oStudentDisability != null)
+                throw new TaskCanceledException("El estudiante ya cuenta una esta discapacidad registrada.");
+
+            StudentDisability _studentDisability = await GetByIdAsync(studentDisability.Id);
+
+            _studentDisability.DisabilityId = studentDisability.DisabilityId;
+
+            return await _repository.UpdateAsync(_studentDisability);
         }
 
-        public Task<bool> DeleteAsync(int id)
+        /// <inheritdoc />
+        public async Task<bool> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _repository.DeleteAsync(id);
         }
     }
 }
