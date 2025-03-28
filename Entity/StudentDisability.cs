2d0334d7f26559719336d9fba87cae443e7f06e9
Dreizack97 <dreizack97@gmail.com>
2025-03-27 20:28:27 -0600

    Creación de objeto 'StudentDisability'.

---
+++ Entity/StudentDisability.cs
@@ -0,0 +1,14 @@
+﻿namespace Entity;
+
+public partial class StudentDisability
+{
+    public int Id { get; set; }
+
+    public int StudentId { get; set; }
+
+    public int DisabilityId { get; set; }
+
+    public virtual Disability Disability { get; set; } = null!;
+
+    public virtual Student Student { get; set; } = null!;
+}
