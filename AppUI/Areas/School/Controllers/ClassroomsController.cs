141cebdf44ea15a0d095f640777c3207de4d8df4
Dreizack97 <dreizack97@gmail.com>
2025-03-29 11:29:42 -0600

    WIP: Desarollo de backend de controlador 'ClassroomsController'.

--- AppUI/Areas/School/Controllers/ClassroomsController.cs
+++ AppUI/Areas/School/Controllers/ClassroomsController.cs
@@ -1,3 +1,7 @@
+using AppUI.Models;
+using AutoMapper;
+using BLL.Interfaces;
+using Entity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +11,74 @@ namespace AppUI.Areas.School.Controllers
     [Area("School")]
     public class ClassroomsController : Controller
     {
+        private readonly IClassroomService _classroomService;
+        private readonly IMapper _mapper;
+
+        public ClassroomsController(IClassroomService classroomService, IMapper mapper)
+        {
+            _classroomService = classroomService;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public async Task<IActionResult> Upsert(int? id)
         {
-            return View();
+            ClassroomModel classroom = id == null ? new ClassroomModel() : _mapper.Map<ClassroomModel>(await _classroomService.GetByIdAsync((int)id));
+            return classroom != null ? View(classroom) : NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClassrooms()
+        {
+            IEnumerable<ClassroomModel> classrooms = _mapper.Map<IEnumerable<ClassroomModel>>(await _classroomService.GetAllAsync());
+            return Json(classrooms);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upsert(ClassroomModel classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (classroom.ClassroomId == 0)
+                    {
+                        await _classroomService.CreateAsync(_mapper.Map<Classroom>(classroom));
+                        TempData["success"] = "Grupo registrado exitosamente.";
+                    }
+                    else
+                    {
+                        await _classroomService.CreateAsync(_mapper.Map<Classroom>(classroom));
+                        TempData["success"] = "Grupo actualizado exitosamente.";
+                    }
+
+                    return View ("Index");
+                }
+                catch (Exception ex)
+                {
+                    TempData["info"] = ex.Message;
+                }
+            }
+
+            return View(classroom);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _classroomService.DeleteAsync(id);
+                return Json(new { success = true, message = "Grupo eliminado exitosamente." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { error = true, message = ex.Message });
+            }
         }
     }
 }
---
+++ AppUI/wwwroot/js/Views/Classrooms_Index.js
@@ -0,0 +1,82 @@
+var dataTable
+
+$(document).ready(async function () {
+    await DataLoad()
+})
+
+async function DataLoad() {
+    dataTable = await $("#dataTable").DataTable({
+        responsive: true,
+        pageLength: 25,
+        ajax: {
+            url: '/School/Classrooms/GetClassrooms',
+            dataSrc: ''
+        },
+        autoWidth: true,
+        columns: [
+            { data: 'classroomId', visible: false, searchable: false },
+            { data: 'name' },
+            { data: 'teacherId' },
+            {
+                data: 'classroomId',
+                render: function (data) {
+                    return `<a class="btn btn-sm btn-primary me-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar" href="/School/Classrooms/Upsert/${data}"><i class="fas fa-pencil-alt"></i></a>
+                    <a class="btn btn-sm btn-danger btn-eliminar" data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar"><i class="fas fa-trash"></i></a>`
+                },
+                orderable: false,
+                searchable: false,
+                width: '100px'
+            }
+        ],
+        order: [[1, "asc"]],
+        dom: "Bfrtip",
+        buttons: [
+            "pageLength",
+        ],
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/2.0.2/i18n/es-MX.json"
+        },
+        drawCallback: function (settings) {
+            const newTooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+            const newTooltipList = [...newTooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+        }
+    })
+}
+
+$("#dataTable tbody").on("click", ".btn-eliminar", async function () {
+    let fila = $(this).closest("tr")
+    const data = dataTable.row(fila).data()
+
+    Swal.fire({
+        title: "¿Deseas eliminar?",
+        text: `Eliminar grupo: ${data.name}.`,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        confirmButtonText: "Eliminar",
+        cancelButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar"
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            $(".swal2-popup").LoadingOverlay("show");
+            await Delete(data.classroomId)
+        }
+    })
+})
+
+async function Delete(classroomId) {
+    await $.ajax({
+        url: `/School/Classroom/Delete/${classroomId}`,
+        type: 'DELETE',
+        success: function (data) {
+            if (data.success) {
+                dataTable.ajax.reload()
+                Swal.fire('', data.message, 'success')
+            } else {
+                Swal.fire('', data.message, 'error')
+            }
+
+            $(".swal2-popup").LoadingOverlay("hide");
+        }
+    })
+}
