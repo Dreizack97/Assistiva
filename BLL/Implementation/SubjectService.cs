b33f3ac586607c390a069b7a08cfcecf6f5c1d1e
Dreizack97 <dreizack97@gmail.com>
2025-04-07 19:37:49 -0600

    Diseño de interfaz e implementación 'SubjectService'.

---
+++ BLL/Implementation/SubjectService.cs
@@ -0,0 +1,41 @@
+﻿using BLL.Interfaces;
+using DAL.Interfaces;
+using Entity;
+
+namespace BLL.Implementation
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly IGenericRepository<Subject> _repository;
+
+        public SubjectService(IGenericRepository<Subject> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Subject> CreateAsync(Subject subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subject> GetByIdAsync(int subjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Subject>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Subject subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int subjectId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Interfaces/ISubjectService.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+
+namespace BLL.Interfaces
+{
+    public interface ISubjectService
+    {
+        Task<Subject> CreateAsync(Subject subject);
+
+        Task<Subject> GetByIdAsync(int subjectId);
+
+        Task<IEnumerable<Subject>> GetAllAsync();
+
+        Task<bool> UpdateAsync(Subject subject);
+
+        Task<bool> DeleteAsync(int subjectId);
+    }
+}
