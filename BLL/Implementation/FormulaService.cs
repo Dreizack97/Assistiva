7b3db3fa7f6ca2846952eb4e5d231e87cf4c26b1
Dreizack97 <dreizack97@gmail.com>
2025-04-08 19:56:55 -0600

    Diseño de interfaz e implementación de 'FormulaService'.

---
+++ BLL/Implementation/FormulaService.cs
@@ -0,0 +1,41 @@
+﻿using BLL.Interfaces;
+using DAL.Interfaces;
+using Entity;
+
+namespace BLL.Implementation
+{
+    public class FormulaService : IFormulaService
+    {
+        private readonly IGenericRepository<Formula> _repository;
+
+        public FormulaService(IGenericRepository<Formula> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Formula> CreateAsync(Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Formula> GetByIdAsync(int formulaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Formula>> GetAllBySubjectIdAsync(int subjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int formulaId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Interfaces/IFormulaService.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+
+namespace BLL.Interfaces
+{
+    public interface IFormulaService
+    {
+        Task<Formula> CreateAsync(Formula formula);
+
+        Task<Formula> GetByIdAsync(int formulaId);
+
+        Task<IEnumerable<Formula>> GetAllBySubjectIdAsync(int subjectId);
+
+        Task<bool> UpdateAsync(Formula formula);
+
+        Task<bool> DeleteAsync(int formulaId);
+    }
+}
