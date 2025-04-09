b3b395176c4ddd001d144aee2b15f96109551f7d
Dreizack97 <dreizack97@gmail.com>
2025-04-08 20:07:02 -0600

    Documentación de interfaz e implementación de 'FormulaService'.

--- BLL/Implementation/FormulaService.cs
+++ BLL/Implementation/FormulaService.cs
@@ -4,15 +4,23 @@ using Entity;
 
 namespace BLL.Implementation
 {
+    /// <summary>
+    /// Implementación del servicio para la gestión de fórmulas matemáticas.
+    /// </summary>
     public class FormulaService : IFormulaService
     {
         private readonly IGenericRepository<Formula> _repository;
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="FormulaService"/>.
+        /// </summary>
+        /// <param name="repository">Repositorio genérico para operaciones CRUD de fórmulas.</param>
         public FormulaService(IGenericRepository<Formula> repository)
         {
             _repository = repository;
         }
 
+        /// <inheritdoc/>
         public async Task<Formula> CreateAsync(Formula formula)
         {
             Formula? oFormula = await _repository.GetByFilterAsync(f => f.Name == formula.Name && f.SubjectId == formula.SubjectId);
@@ -28,17 +36,20 @@ namespace BLL.Implementation
             return _formula;
         }
 
+        /// <inheritdoc/>
         public async Task<Formula> GetByIdAsync(int formulaId)
         {
             return await _repository.GetByIdAsync(formulaId)
                 ?? throw new TaskCanceledException("No se ha encontrado una fórmula con la información proporcionada.");
         }
 
+        /// <inheritdoc/>
         public async Task<IEnumerable<Formula>> GetAllBySubjectIdAsync(int subjectId)
         {
             return await _repository.GetAllAsync(f => f.SubjectId == subjectId);
         }
 
+        /// <inheritdoc/>
         public async Task<bool> UpdateAsync(Formula formula)
         {
             Formula? oFormula = await _repository.GetByFilterAsync(f => f.Name == formula.Name && f.SubjectId == formula.SubjectId && f.FormulaId != formula.FormulaId);
@@ -55,6 +66,7 @@ namespace BLL.Implementation
             return await _repository.UpdateAsync(_formula);
         }
 
+        /// <inheritdoc/>
         public async Task<bool> DeleteAsync(int formulaId)
         {
             return await _repository.DeleteAsync(formulaId);
