8d717710f0789739aae22680606bac54b41e9aee
Dreizack97 <dreizack97@gmail.com>
2025-04-08 20:01:44 -0600

    Desarrollo de la implementación del servicio 'FormulaService'.

--- BLL/Implementation/FormulaService.cs
+++ BLL/Implementation/FormulaService.cs
@@ -13,29 +13,51 @@ namespace BLL.Implementation
             _repository = repository;
         }
 
-        public Task<Formula> CreateAsync(Formula formula)
+        public async Task<Formula> CreateAsync(Formula formula)
         {
-            throw new NotImplementedException();
+            Formula? oFormula = await _repository.GetByFilterAsync(f => f.Name == formula.Name && f.SubjectId == formula.SubjectId);
+
+            if (oFormula != null)
+                throw new TaskCanceledException("Ya existe una fórmula con el mismo nombre para la misma asignatura.");
+
+            Formula _formula = await _repository.AddAsync(formula);
+
+            if (_formula.FormulaId == 0)
+                throw new TaskCanceledException("Ocurrió un error al intentar registrar la fórmula.");
+
+            return _formula;
         }
 
-        public Task<Formula> GetByIdAsync(int formulaId)
+        public async Task<Formula> GetByIdAsync(int formulaId)
         {
-            throw new NotImplementedException();
+            return await _repository.GetByIdAsync(formulaId)
+                ?? throw new TaskCanceledException("No se ha encontrado una fórmula con la información proporcionada.");
         }
 
-        public Task<IEnumerable<Formula>> GetAllBySubjectIdAsync(int subjectId)
+        public async Task<IEnumerable<Formula>> GetAllBySubjectIdAsync(int subjectId)
         {
-            throw new NotImplementedException();
+            return await _repository.GetAllAsync(f => f.SubjectId == subjectId);
         }
 
-        public Task<bool> UpdateAsync(Formula formula)
+        public async Task<bool> UpdateAsync(Formula formula)
         {
-            throw new NotImplementedException();
+            Formula? oFormula = await _repository.GetByFilterAsync(f => f.Name == formula.Name && f.SubjectId == formula.SubjectId && f.FormulaId != formula.FormulaId);
+
+            if (oFormula != null)
+                throw new TaskCanceledException("Ya existe una fórmula con el mismo nombre para la misma asignatura.");
+
+            Formula _formula = await GetByIdAsync(formula.FormulaId);
+
+            _formula.Name = formula.Name;
+            _formula.Content = formula.Content;
+            _formula.Description = formula.Description;
+
+            return await _repository.UpdateAsync(_formula);
         }
 
-        public Task<bool> DeleteAsync(int formulaId)
+        public async Task<bool> DeleteAsync(int formulaId)
         {
-            throw new NotImplementedException();
+            return await _repository.DeleteAsync(formulaId);
         }
     }
 }
