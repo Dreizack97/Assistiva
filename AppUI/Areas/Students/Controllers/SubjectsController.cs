c88e31fe90d3dc14a0138f7947da0ae64d92b1e4
Dreizack97 <dreizack97@gmail.com>
2025-04-15 19:52:04 -0600

    Creación de controlador 'SubjectsController'.

---
+++ AppUI/Areas/Students/Controllers/SubjectsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppUI.Areas.Students.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Subject()
+        {
+            return View();
+        }
+    }
+}
