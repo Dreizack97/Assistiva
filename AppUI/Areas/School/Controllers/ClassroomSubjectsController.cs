17612f0ce9e1a4a2dd45fef12e49bcb5c0103694
Dreizack97 <dreizack97@gmail.com>
2025-04-17 19:14:07 -0600

    Creación de controlador 'ClassroomSubjectsController'.

---
+++ AppUI/Areas/School/Controllers/ClassroomSubjectsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppUI.Areas.School.Controllers
+{
+    [Authorize]
+    [Area("School")]
+    public class ClassroomSubjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+    }
+}
