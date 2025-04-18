475ee1404a41ee65418e6c89a3b5e4a3c44f3abe
Dreizack97 <dreizack97@gmail.com>
2025-04-17 19:08:10 -0600

    Creación de modelo 'ClassroomSubjectModel'.

---
+++ AppUI/Models/ClassroomSubjectModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppUI.Models
+{
+    public class ClassroomSubjectModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Selecciona el grupo de clases.")]
+        [DisplayName("Grupo")]
+        public int ClassroomId { get; set; }
+
+        [Required(ErrorMessage = "Selecciona la materia.")]
+        [DisplayName("Materia")]
+        public int SubjectId { get; set; }
+    }
+}
